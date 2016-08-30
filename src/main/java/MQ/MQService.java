855256cf3a19e4efe758ac888c394ec67b088ef3
Jason Chen <chenhao86899@gmail.com>
2016-08-30 17:49:29 +0800

    MQ V1

--- src/main/java/MQ/MQService.java
+++ src/main/java/MQ/MQService.java
@@ -5,15 +5,38 @@ package MQ;
  */
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.test.context.ContextConfiguration;
+import org.zeromq.ZMQ;
 
 /**
- * 此客户端可以和Spring进行结合,当然无法和Spring的Jms进行结合
+ * 此客户端可以和Spring进行结合
  */
-
-@ContextConfiguration(locations = "classpath:applicationContext.xml")
+@Component
+//@ContextConfiguration(locations = "classpath:applicationContext.xml")
 public class MQService extends Thread {
-    private int type;
+    @Autowired
+    private BrokerPullSingleton brokerPullSingleton;
+    private String ServiceAddress;
+    private ZMQ.Context context;
+    private ZMQ.Socket transfer;
+    public MQService(int type,String adr){
+        this.ServiceAddress = adr;
+        this.context = ZMQ.context(1);
+        this.transfer = context.socket(type);
+    }
+    public void run(){
+        brokerPullSingleton.start();
+        String key =null;
+        while (true) {
+            if ((key = transfer.recvStr()).contains("Queue"))
+                new BrokerPush(ServiceAddress, ZMQ.PUSH, MessageQueueMap.getByName(key)).start();
+            if ((key = transfer.recvStr()).contains("Topic"))
+                new BrokerPush(ServiceAddress, ZMQ.PUB, key, MessageQueueMap.getByName(key)).start();
+        }
+    }
+
 
 
 
